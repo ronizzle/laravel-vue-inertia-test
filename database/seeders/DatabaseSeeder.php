c24020b42581fb80f2473c71166b7ad64f93b718
Niel B <ronieldevmail@gmail.com>
2024-10-02 14:57:20 +0800

    Add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,25 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Services\TypicodeService;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    public function __construct(
+        private readonly TypicodeService $typicodeService
+    )
+    {
+
+    }
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->typicodeService->depopulateTypicodeTables();;
+        $this->typicodeService->syncUsers();
+        $this->typicodeService->syncPosts();
     }
 }
